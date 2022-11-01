a4592d295ba5732450a0383fcb6384a751270149
Yana Bialkova <yana.bialkova@gmail.com>
2022-11-01 14:25:31 +0100

    Add files for work

---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,149 @@
+package data;
+
+import com.github.javafaker.Faker;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataHelper {
+
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private static int validYear = Integer.parseInt(getCurrentYear()) + 1;
+    private static String numberApprovedCard = "4444 4444 4444 4441";
+    private static String numberDeclinedCard = "4444 4444 4444 4442";
+
+
+    public static CardInfo generateDataWithApprovedCard() {
+        var randomName = faker.name().fullName();
+        var randomCvc = faker.number().digits(3);
+        return new CardInfo(numberApprovedCard, getCurrentMonth(), String.valueOf(validYear), randomName, randomCvc);
+    }
+
+    public static CardInfo generateDataWithDeclineCard() {
+
+        var randomName = faker.name().fullName();
+        var randomCvc = faker.number().digits(3);
+        return new CardInfo(numberDeclinedCard, getCurrentMonth(), String.valueOf(validYear), randomName, randomCvc);
+    }
+
+    public static CardInfo generateDataWithRandomCardNumber() {
+        var randomName = faker.name().fullName();
+        var randomCardNumber = faker.number().digits(16);
+        var randomCvc = faker.number().digits(3);
+        return  new CardInfo(randomCardNumber, getCurrentMonth(), String.valueOf(validYear), randomName, randomCvc);
+    }
+
+    public static CardInfo generateDataWithApprovedCardAndParametrizedMonthAndYear(String month, String year) {
+        var randomName = faker.name().fullName();
+        var randomCvc = faker.number().digits(3);
+        return  new CardInfo(numberApprovedCard, month, year, randomName, randomCvc);
+
+    }
+
+    public static CardInfo generateDataWithParamLengthCardOwnerName(int length) {
+        var randomName = faker.lorem().fixedString(length);
+        var randomCvc = faker.number().digits(3);
+        return new CardInfo(numberApprovedCard, getCurrentMonth(), String.valueOf(validYear), randomName, randomCvc);
+    }
+
+    public static CardInfo generateDataWithParamCardOwnerName(String name) {
+        var randomCvc = faker.number().digits(3);
+        return new CardInfo(numberApprovedCard, getCurrentMonth(), String.valueOf(validYear), name, randomCvc);
+    }
+
+    public static CardInfo generateDataExpiredCardForOneMonth() {
+        var randomName = faker.name().fullName();
+        var randomCvc = faker.number().digits(3);
+        var currentMonth = Integer.parseInt(getCurrentMonth());
+        var currentYear = Integer.parseInt(getCurrentYear());
+        if (currentMonth == 1) {
+            currentMonth = 12;
+            currentYear = currentYear - 1;
+        } else currentMonth = currentMonth - 1;
+
+        String minusOneFromCurrentMonth = "";
+        if (currentMonth < 10) {
+            minusOneFromCurrentMonth = "0" + currentMonth;
+        }
+        return  new CardInfo(numberApprovedCard, minusOneFromCurrentMonth,
+                String.valueOf(currentYear), randomName, randomCvc);
+    }
+
+    public static CardInfo generateDataWithMaxDateMinusOneMonth() {
+        var randomName = faker.name().fullName();
+        var randomCvc = faker.number().digits(3);
+        var currentMonth = Integer.parseInt(getCurrentMonth());
+        var preMaxMonth = 0;
+        var maxYear = Integer.parseInt(getCurrentYear()) + 5;
+
+        if (currentMonth == 1) {
+            preMaxMonth = 12;
+            maxYear = maxYear - 1;
+        } else preMaxMonth = currentMonth - 1;
+
+        String strPreMaxMonth = "";
+        if (preMaxMonth < 10) {
+            strPreMaxMonth = "0" + preMaxMonth;
+        }
+        return  new CardInfo(numberApprovedCard, strPreMaxMonth,
+                String.valueOf(maxYear), randomName, randomCvc);
+    }
+
+    public static String getCurrentMonth() {
+        LocalDate date = LocalDate.now();
+        String currentMonth = date.format(DateTimeFormatter.ofPattern("MM"));
+        return currentMonth;
+    }
+
+    public static String getCurrentYear() {
+        LocalDate date = LocalDate.now();
+        String currentYear = date.format(DateTimeFormatter.ofPattern("yy"));
+        return currentYear;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CardInfo {
+        String number;
+        String month;
+        String year;
+        String holder;
+        String cvc;
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreditCardData {
+        private String id;
+        private String bank_id; //пока будем брать данные из поля bank_id вместо id
+        private String created;
+        private String status;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PaymentCardData {
+        private String id;
+        private String amount;
+        private String created;
+        private String status;
+        private String transaction_id; //пока будем брать данные из поля transaction_id вместо id
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TableOrderEntity {
+        private String id;
+        private String created;
+        private String credit_id;
+        private String payment_id;
+    }
+}
---
+++ src/test/java/data/SQLHelper.java
@@ -0,0 +1,76 @@
+package data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class SQLHelper {
+
+    private static QueryRunner runner  = new QueryRunner();
+    private static  String url = System.getProperty("db.url");
+    private static  String userName = System.getProperty("db.username");
+    private static  String password = System.getProperty("db.password");
+//     private static String url = "jdbc:mysql://localhost:3306/app";
+//    private static String url = "jdbc:postgresql://localhost:5432/app";
+
+
+    public SQLHelper(){
+    }
+
+
+    @SneakyThrows
+    private static Connection getConn() {
+        return DriverManager.getConnection(url, userName, password);
+    }
+
+
+    public static DataHelper.CreditCardData getCreditCardData() {
+        var cardDataSQL =  "SELECT * FROM credit_request_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var result = runner.query(conn, cardDataSQL,
+                    new BeanHandler<>(DataHelper.CreditCardData.class));
+            return result;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public static DataHelper.PaymentCardData getPaymentCardData() {
+        var cardDataSQL =  "SELECT * FROM payment_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var result = runner.query(conn, cardDataSQL,
+                    new BeanHandler<>(DataHelper.PaymentCardData.class));
+            return result;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public static DataHelper.TableOrderEntity getTableOrderEntity() {
+        var orderEntityDataSQL =  "SELECT * FROM order_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var result = runner.query(conn, orderEntityDataSQL,
+                    new BeanHandler<>(DataHelper.TableOrderEntity.class));
+            return result;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @SneakyThrows
+    public static void cleanDatabase() {
+        var conn = getConn();
+        runner.execute(conn, "DELETE FROM order_entity");
+        runner.execute(conn, "DELETE FROM payment_entity");
+        runner.execute(conn, "DELETE FROM credit_request_entity");
+    }
+}
